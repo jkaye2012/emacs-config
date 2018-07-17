36dde0035037d1fecdacf51bab8c2a3e1e2bb29f
Jordan Kaye <jkaye@belvederetrading.com>
2018-07-17 12:38:07 -0500

    Me webdev now

--- init.el
+++ init.el
@@ -23,6 +23,7 @@
 (setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin"))
 (setq-default indent-tabs-mode nil)
 (menu-bar-mode -1)
+(setq compilation-scroll-output t)
 
 (defun Fuco1/lisp-indent-function (indent-point state)
   "This function is the normal value of the variable `lisp-indent-function'.
@@ -172,6 +173,10 @@ Lisp function does not specify a special indentation."
     ("s" sp-forward-slurp-sexp "slurp forward")
     ("W" sp-unwrap-sexp "unwrap"))
 
+  (defhydra hydra-tide-reference ()
+    "Navigate references"
+    ("n" tide-find-next-reference "next")
+    ("p" tide-find-previous-reference "previous"))
   )
 
 (defun my/switch-to-previous-buffer ()
@@ -287,6 +292,8 @@ Repeated invocations toggle between the two most recently open buffers."
   :config
   (add-hook 'python-mode-hook #'(lambda () (setq flycheck-checker 'python-pylint)))
   (add-hook 'python-mode-hook 'flycheck-mode)
+  (add-hook 'js2-mode-hook #'(lambda () (setq flycheck-checker 'javascript-jshint)))
+  (add-hook 'js2-mode-hook 'flycheck-mode)
 
   (general-define-key
    :states '(normal visual insert emacs)
@@ -436,6 +443,91 @@ is achieved by adding the relevant text properties."
 
 (provide 'ebnf-mode)
 
+(use-package docker)
+
+(use-package dockerfile-mode)
+
+(use-package docker-compose-mode)
+
+(use-package npm-mode
+  :config
+  (add-hook 'js2-mode-hook 'npm-mode)
+  (add-hook 'tide-mode-hook 'npm-mode))
+
+(use-package tide
+  :config
+  (defun setup-tide-mode ()
+    (interactive)
+    (tide-setup)
+    (flycheck-mode +1)
+    (setq flycheck-check-syntax-automatically '(save mode-enabled))
+    (eldoc-mode +1)
+    (tide-hl-identifier-mode +1)
+    (company-mode +1)
+    (setq company-tooltip-align-annotations t))
+  (add-hook 'before-save-hook 'tide-format-before-save)
+  (add-hook 'typescript-mode-hook #'setup-tide-mode)
+
+  (use-package web-mode
+    :config
+    (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+    (add-hook 'web-mode-hook
+              (lambda ()
+                (when (string-equal "tsx" (file-name-extension buffer-file-name))
+                  (setup-tide-mode))))
+    ;; enable typescript-tslint checker
+    (flycheck-add-mode 'typescript-tslint 'web-mode)
+    (add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
+    (add-hook 'web-mode-hook
+              (lambda ()
+                (when (string-equal "jsx" (file-name-extension buffer-file-name))
+                  (setup-tide-mode))))
+    ;; configure jsx-tide checker to run after your default jsx checker
+    (flycheck-add-mode 'javascript-eslint 'web-mode)
+    (flycheck-add-next-checker 'javascript-eslint 'jsx-tide 'append)
+    )
+
+  (add-hook 'js2-mode-hook #'setup-tide-mode)
+  ;; configure javascript-tide checker to run after your default javascript checker
+  (flycheck-add-next-checker 'javascript-eslint 'javascript-tide 'append)
+
+  (defun my/tide-navigate ()
+    (interactive)
+    (tide-references)
+    (select-window (get-buffer-window "*tide-references*")))
+
+  (defun my/tide-goto ()
+    (interactive)
+    (tide-goto-reference))
+
+  (general-define-key
+   :states '(normal)
+   :keymaps '(tide-references-mode-map)
+    "<RET>" '(my/tide-goto :wk "goto reference")
+    "n" '(tide-find-next-reference :wk "next")
+    "N" '(tide-find-previous-reference :wk "previous"))
+
+  (general-define-key
+   :states '(normal)
+   :keymaps '(tide-mode-map)
+   :prefix ","
+    "d" '(tide-documentation-at-point :wk "documentation at point")
+    "g" '(tide-jump-to-definition :wk "jump to definition")
+    "r" '(my/tide-navigate :wk "navigate references")
+    ))
+
+(use-package js2-mode
+  :config
+  (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+  (use-package xref-js2
+    :config
+    (add-hook 'js2-mode-hook (lambda ()
+                               (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t))))
+  (use-package company-tern
+    :config
+    (add-to-list 'company-backends 'company-tern)
+    (add-hook 'js2-mode-hook 'tern-mode)))
+
 (use-package cmake-mode)
 
 (use-package irony
@@ -515,13 +607,13 @@ is achieved by adding the relevant text properties."
     ("84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "c9ddf33b383e74dac7690255dd2c3dfa1961a8e8a1d20e401c6572febef61045" "36ca8f60565af20ef4f30783aa16a26d96c02df7b4e54e9900a5138fb33808da" default)))
  '(package-selected-packages
    (quote
-    (evil-org smex w3m counsel-dash multi-term counsel-projectile counsel racer cmake-mode rust-mode evil-visualstar flycheck-rtags rtags flycheck-irony company-irony irony evil-matchit yasnippet-snippets yasnippet evil rainbow-delimiters evil-magit magit smart-mode-line-powerline-theme smart-mode-line eshell-prompt-extras nose virtualenvwrapper pyenv-mode avy anaconda-mode ample-theme flycheck which-key smartparens use-package)))
+    (web-mode tide company-tern xref-js2 js2-mode npm-mode docker docker-mode docker-compose-mode dockerfile-mode evil-org smex w3m counsel-dash multi-term counsel-projectile counsel racer cmake-mode rust-mode evil-visualstar flycheck-rtags rtags flycheck-irony company-irony irony evil-matchit yasnippet-snippets yasnippet evil rainbow-delimiters evil-magit magit smart-mode-line-powerline-theme smart-mode-line eshell-prompt-extras nose virtualenvwrapper pyenv-mode avy anaconda-mode ample-theme flycheck which-key smartparens use-package)))
  '(safe-local-variable-values
    (quote
     ((eval setq cmake-ide-build-dir
-	   (concat
-	    (projectile-project-root)
-	    "build")))))
+           (concat
+            (projectile-project-root)
+            "build")))))
  '(term-unbind-key-list (quote ("C-z" "C-x" "C-c" "C-h" "C-y" "<ESC>"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
