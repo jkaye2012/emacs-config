5df7e522976c448da70e5bf8de1b69f4fde74b50
Jordan Kaye <jordan.kaye2@gmail.com>
2019-10-04 08:40:37 -0600

    Craft-mode modifications

--- personal/craft-mode.el
+++ personal/craft-mode.el
@@ -1,12 +1,14 @@
 
 (setq craft/font-lock
       '(
-        ("#.*\n" . font-lock-comment-face)
-        ("[ \n]\\(data\\|concept\\|class\\|def\\|match\\|with\\|extern\\)[ \n]" . font-lock-keyword-face)
+        ("#.*\n?" . font-lock-comment-face)
+        ("[ \n]?\\(data\\|concept\\|class\\|def\\|match\\|with\\|extern\\|let\\)[ \n]" . font-lock-keyword-face)
         ("self\\|true\\|false" . font-lock-constant-face)
-        ("[A-Z]\\([A-Za-z]\\)+" . font-lock-type-face)
+        ("[A-Z]\\([A-Za-z]\\)+" . font-lock-constant-face)
         ("[A-Z]" . font-lock-variable-name-face)
-        ("[a-z]\\([a-z_]\\)+" . font-lock-function-name-face)
+        ("[a-z]\\([a-z_]\\)+(" . font-lock-function-name-face)
+        ("[[:digit:]]+[fdils]?" . font-lock-builtin-face)
+        ("_?[a-z][a-z_]*" . font-lock-variable-name-face)
         ))
 
 (define-derived-mode craft-mode prog-mode "craft"
