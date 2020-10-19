21490913c19cef5d0d35e189fda553c46beca4bc
Jordan Kaye <jkaye@belvederetrading.com>
2020-10-19 10:17:08 -0600

    Typescript uses lsp

--- custom.el
+++ custom.el
@@ -52,6 +52,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((t (:background nil))))
  '(flycheck-error ((t (:background "gray" :underline (:color "#D70000" :style wave))))))
 
 (put 'erase-buffer 'disabled nil)
--- personal/typescript.el
+++ personal/typescript.el
@@ -1,29 +1,5 @@
 
-(use-package tide
+(use-package typescript-mode
+  :hook ((typescript-mode . lsp))
   :config
-  (defun setup-tide-mode ()
-    (interactive)
-    (tide-setup)
-    (flycheck-mode +1)
-    (setq flycheck-check-syntax-automatically '(save mode-enabled))
-    (eldoc-mode +1)
-    (tide-hl-identifier-mode +1)
-    ;; company is an optional dependency. You have to
-    ;; install it separately via package-install
-    ;; `M-x package-install [ret] company`
-    (company-mode +1))
-
-  ;; aligns annotation to the right hand side
-  (setq company-tooltip-align-annotations t)
-
-  (add-hook 'typescript-mode-hook #'setup-tide-mode)
-
-  (require 'web-mode)
-  (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
-  (add-hook 'web-mode-hook
-            (lambda ()
-              (when (string-equal "tsx" (file-name-extension buffer-file-name))
-                (setup-tide-mode))))
-  ;; enable typescript-tslint checker
-  (flycheck-add-mode 'typescript-tslint 'web-mode)
-  )
+  (add-to-list 'auto-mode-alist '("\\.tsx\\'" . typescript-mode)))
