be18605faaab404464d53c76c4b531d5b59be92e
Jordan Kaye <jkaye@belvederetrading.com>
2020-10-21 07:45:05 -0600

    Undo-tree integration

---
+++ personal/undo.el
@@ -0,0 +1,27 @@
+
+(use-package undo-tree
+  :config
+  (global-undo-tree-mode)
+
+  (general-define-key
+   :states '(normal)
+    "u" '(undo-tree-undo :wk "undo")
+    "C-r" '(undo-tree-redo :wk "redo"))
+
+  (general-define-key
+   :states '(normal)
+   :prefix "SPC"
+    "u" '(nil :wk "Undo")
+    "ur" '(undo-tree-restore-state-from-register :wk "restore")
+    "us" '(undo-tree-save-state-to-register :wk "save")
+    "uv" '(undo-tree-visualize :wk "visualize"))
+
+  (general-define-key
+   :states '(normal)
+   :keymaps '(undo-tree-visualizer-mode-map)
+    "d" '(undo-tree-visualizer-toggle-diff :wk "diffs")
+    "h" '(undo-tree-visualize-switch-branch-left :wk "left branch")
+    "l" '(undo-tree-visualize-switch-branch-right :wk "right branch")
+    "t" '(undo-tree-visualizer-toggle-timestamps :wk "timestamps")
+    "n" '(undo-tree-visualize-redo-to-x :wk "next branch")
+    "u" '(undo-tree-visualize-undo-to-x :wk "last branch")))
